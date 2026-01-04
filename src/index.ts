b63e7eea7f85283b9f5a947886ebb1cf8918e477
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-01-04 13:59:18 +0100

    feat: rhidium submodule compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,13 @@
-import { GatewayIntentBits } from 'discord.js';
-import { appConfig } from './config';
-import Client from 'rhidium/core/client';
-import { I18n } from 'rhidium/core/i18n';
-import { commandDeploymentEnvironment } from 'rhidium/core/commands/defaults';
-import { ClientManager } from 'rhidium/core/commands/manager';
+import { main } from 'rhidium/main'
 
-const main = async () => {
-  await I18n.init();
-  const manager = new ClientManager();
-  const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
-    manager,
-  });
+import utilityRegistry from 'rhidium/modules/utility';
+import systemRegistry from 'rhidium/modules/system';
+import moderationRegistry from 'rhidium/modules/moderation';
 
-  manager.register(
-    // ...systemRegistry,
-    // ...utilityRegistry,
-    // ...moderationRegistry,
-  );
-
-  await Promise.all([
-    client.login(appConfig.client.token, {
-      guildId: commandDeploymentEnvironment,
-      clearOtherEnvironment: process.env['NODE_ENV'] !== 'production',
-      forceSync: false,
-    }),
-  ]);
-};
-
-void main();
+main({
+  components: [
+    ...systemRegistry,
+    ...utilityRegistry,
+    ...moderationRegistry,
+  ]
+})
