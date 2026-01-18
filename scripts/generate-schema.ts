0b35b2872f7a4eb55818c078f723f43e3411cae7
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-01-18 17:33:39 +0100

    feat: permissions completeness

--- scripts/generate-schema.ts
+++ scripts/generate-schema.ts
@@ -2,6 +2,9 @@ import { writeFileSync, mkdirSync } from 'fs';
 import path from 'path';
 import { clickToCreateSchema } from '../src/schema/schemas';
 
+// Note: The exported schema is copied and used in mirasaki.dev/docs.
+// If any changes are made to the schema, please remember to update the docs as well.
+
 const logger = console;
 
 const jsonSchema = clickToCreateSchema.toJSONSchema({
